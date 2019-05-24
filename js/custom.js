77ef2cba583fb4058f866ab63353593e69d42635
edlamahendar <45842094+edlamahendar@users.noreply.github.com>
2019-05-24 14:11:19 +0530

    Create custom.js

---
+++ js/custom.js
@@ -0,0 +1,45 @@
+// Wait for the DOM to be ready
+$(function() {
+  // Initialize form validation on the registration form.
+  // It has the name attribute "registration"
+  $("form[name='education']").validate({
+    // Specify validation rules
+    rules: {
+      // The key name on the left side is the name attribute
+      // of an input field. Validation rules are defined
+      // on the right side
+      FirstName: "required",
+      LastName: "required",
+	  wantToapply:"required",
+	  sbirthDay:"required",
+	  ParentName:"required",
+	  PhoneNumber:"required",
+	  Adddress:"required",
+      Email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        Email: true
+      }
+	  
+      
+    },
+    // Specify validation error messages
+    messages: {
+      FirstName: "Please enter your firstname",
+      LastName: "Please enter your lastname",
+	  wantToapply: "Please enter Class you want to apply for",
+      sbirthDay: "Please enter Student's birth Day",
+	  ParentName: "Please enter your Parent Name",
+	  PhoneNumber: "Please enter your Phone Number",
+	  Adddress: "Please enter your Adddress",
+      Email: "Please enter a valid email address"
+    },
+	
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
